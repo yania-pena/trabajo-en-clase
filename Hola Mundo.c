95ca2da16767972e13044836c441dad920a4787d
yania-pena <55764750+yania-pena@users.noreply.github.com>
2019-10-03 15:20:36 -0500

    Add files via upload

---
+++ Hola Mundo.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("*  *  ****  *    ****      *   *  *  *  *    *  ***    ****\n");
+    printf("*  *  *  *  *    *  *      * * *  *  *  * *  *  *  *   *  *\n");
+    printf("****  *  *  *    ****      *   *  *  *  *  * *  *   *  *  *\n");
+    printf("*  *  *  *  *    *  *      *   *  *  *  *   **  *  *   *  *\n");
+    printf("*  *  ****  **** *  *      *   *  ****  *    *  ***    ****\n");
+}
+//vamos separando los "*" depende de la palabra que queremos formar y por cada fila vamos realizando un salto de linea(\n)
+//Y al final imprimimos la palabra formada que es "HOLA MUNDO"
+
+
